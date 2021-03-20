2ddbf955b6302bef1d30b4123ed17738fe51933e
Oleh <oleh.m97@gmail.com>
2021-03-20 14:19:19 +0200

    update

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,28 @@
-const express = rquire('express');
-
+const express = require('express');
+const useSocket = require('socket.io')
 const app = express();
+const server = require('http').Server(app)
+const io = useSocket(server);
 
-const rooms = {
-    rooms: [],
-    messages: ['hello']
-}
-app.get('/users', function (req, res) {
+const rooms = new Map([
+    ['rooms', []],
+    ['messages', []]
+])
+app.get('/rooms', function (req, res) {
     console.log('hello')
+    rooms.set('hello', '')
     res.json('')
 
 });
 
+io.on('connection', socket => {
+    console.log('socked connected', socket)
+})
 
-app.listen(9999);
+
+app.listen(9999, (err) => {
+    if (err) {
+        throw Error(err)
+    }
+    console.log(`Server has been started`)
+});
