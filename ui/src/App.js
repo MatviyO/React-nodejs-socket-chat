381495d1e18f52d050d0e83ce4327f7e9475ca6a
Oleh <oleh.m97@gmail.com>
2021-04-03 19:02:20 +0300

    up

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,5 +1,6 @@
 import React, {useReducer} from 'react'
 import Login from "./components/Login";
+import reducer from "./reducer";
 
 function App() {
     const [state, dispatch] = useReducer(reducer, {
@@ -11,9 +12,10 @@ function App() {
             payload: true
         })
     }
+    console.log(state)
 
-    
-    
+
+
     return (
         <div className="container mt-5">
             {<Login onLogin={onLogin}/>}
